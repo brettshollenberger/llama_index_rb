646afa7a0f86aa36d36dd14e7985ff4af8bfb86c
Brett Shollenberger <brettshollenberger@Bretts-MacBook-Pro-2.local>
2024-07-03 19:23:57 -0400

    Add Callbacks + tests

---
+++ lib/llama_index_rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "llama_index_rb/version"
+
+module LlamaIndexRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/llama_index_rb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LlamaIndexRb
+  VERSION = "0.1.0"
+end
